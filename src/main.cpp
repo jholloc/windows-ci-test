4587a0633ec3e7bacbd7bfb9e2e04eb9559151e9
Jonathan Hollocombe <jonathan.hollocombe@ukaea.uk>
2023-07-21 19:20:22 +0100

    Attempting to solve capnp issue.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-#include <libxml/xmlversion.h>
+//#include <libxml/xmlversion.h>
 #include <capnp/common.h>
-#include <openssl/opensslv.h>
-#include <spdlog/version.h>
-#include <boost/version.hpp>
+//#include <openssl/opensslv.h>
+//#include <spdlog/version.h>
+//#include <boost/version.hpp>
 
 int main()
 {
-    std::cout << "libxml2 version = " << LIBXML_DOTTED_VERSION << std::endl;
+//    std::cout << "libxml2 version = " << LIBXML_DOTTED_VERSION << std::endl;
     std::cout << "capnp version = " << CAPNP_VERSION << std::endl;
-    std::cout << "openssl version = " << OPENSSL_VERSION_TEXT << std::endl;
-    std::cout << "spdlog version = " << SPDLOG_VERSION << std::endl;
-    std::cout << "boost version = " << BOOST_LIB_VERSION << std::endl;
+//    std::cout << "openssl version = " << OPENSSL_VERSION_TEXT << std::endl;
+//    std::cout << "spdlog version = " << SPDLOG_VERSION << std::endl;
+//    std::cout << "boost version = " << BOOST_LIB_VERSION << std::endl;
     return 0;
 }
