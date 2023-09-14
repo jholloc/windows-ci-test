2ff48f9d3a0fc4dbdf386184ef686217247f9b14
Jonathan Hollocombe <jonathan.hollocombe@ukaea.uk>
2023-09-14 11:16:44 +0100

    Adding program options to main

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <openssl/opensslv.h>
 #include <spdlog/version.h>
 #include <boost/version.hpp>
+#include <boost/program_options.hpp>
 #include <sys/time.h>
 #include <fmt/format.h>
 
@@ -23,8 +24,37 @@ LIBRARY_API int gettimeofday(struct timeval* tp, struct timezone* tzp);
 #endif
 #endif
 
-int main()
+namespace po = boost::program_options;
+
+int main(int argc, const char** argv)
 {
+    po::options_description desc("Allowed options");
+    desc.add_options()
+            ("help", "produce help message");
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).options(desc).run(), vm);
+    try {
+        po::notify(vm);
+    } catch (po::error& err) {
+        if (vm.count("help")) {
+            std::cout << "Usage: " << argv[0] << " [options] request\n";
+            std::cout << desc << "\n";
+            return 1;
+        } else {
+            std::cout << "Error: " << err.what() << "\n\n";
+            std::cout << "Usage: " << argv[0] << " [options] request\n";
+            std::cout << desc << "\n";
+            return -1;
+        }
+    };
+
+    if (vm.count("help")) {
+        std::cout << "Usage: " << argv[0] << " [options] request\n";
+        std::cout << desc << "\n";
+        return 1;
+    }
+
     std::cout << "libxml2 version = " << LIBXML_DOTTED_VERSION << std::endl;
     std::cout << "capnp version = " << CAPNP_VERSION << std::endl;
     std::cout << "openssl version = " << OPENSSL_VERSION_TEXT << std::endl;
